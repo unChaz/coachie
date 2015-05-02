18cbadd52defc39b243eed634e6a4629db78678a
Eric Martindale <eric@ericmartindale.com>
2015-05-02 19:39:54 -0400

    Add missing config.

---
+++ config/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  service: {
+    name: 'coachie',
+    mission: 'get coached in esports',
+    description: 'awesome esports are awesome and you\'re not.  get better.  coachi.',
+    points: [
+      { header: 'lolwat' },
+      { header: 'lolwat' },
+      {
+        header: 'Already registered?',
+        description: 'Go on then.  Get logged in.  You\'re _groovy_.',
+        action: {
+          text: 'Log In &raquo;',
+          link: '/sessions'
+        }
+      }
+    ]
+  },
+  database: {
+    name: 'chaz'
+  }
+};
