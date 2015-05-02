7a3ac52ae8821349ea05708016643ca2b7afb122
Eric Martindale <eric@ericmartindale.com>
2015-05-02 19:36:21 -0400

    Add new config file.

--- coachie.js
+++ coachie.js
@@ -1,26 +1,7 @@
+var config = require('./config');
+
 var Maki = require('maki');
-var chaz = new Maki({
-  service: {
-    name: 'coachie',
-    mission: 'get coached in esports',
-    description: 'awesome esports are awesome and you\'re not.  get better.  coachi.',
-    points: [
-      { header: 'lolwat' },
-      { header: 'lolwat' },
-      {
-        header: 'Already registered?',
-        description: 'Go on then.  Get logged in.  You\'re _groovy_.',
-        action: {
-          text: 'Log In &raquo;',
-          link: '/sessions'
-        }
-      }
-    ]
-  },
-  database: {
-    name: 'chaz'
-  }
-});
+var chaz = new Maki( config );
 
 var Passport = require('maki-passport-local');
 var passport = new Passport({
